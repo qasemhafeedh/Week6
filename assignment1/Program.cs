54a3d4c5087f197de29699b220bdb1fcf99d8a0d
qasemhafeedh <712228@student.inholland.nl>
2024-11-14 10:39:59 +0100

    Add project files.

---
+++ assignment1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Program program = new Program();
+            program.Start();
+        }
+        void Start()
+        {
+            Console.Write("Enter name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter age: ");
+            int age = int .Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Person person = new Person(age, name);
+            person.Display();
+        }
+    }
+}
